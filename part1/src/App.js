f3b0abab4ed43978ce3478c6c4a209724d9061f0
Kenny <vuvu123@gmail.com>
2022-09-27 08:52:00 -0700

    Exercise 1.8 complete

--- part1/src/App.js
+++ part1/src/App.js
@@ -6,19 +6,28 @@ const Button = ( {handleClick, text} ) => {
   return <button onClick={handleClick}>{text}</button>
 }
 
-const Statistic = ( {type, count} ) => <p>{type} {count}</p>
+const Statistics = ( {type, count} ) => <div>{type} {count}</div>
 
 const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
 
-  const addGoodFeedback = () => {
-    setGood(good + 1)
-  }
-
+  const addGoodFeedback = () => setGood(good + 1)
   const addNeutralFeedback = () => setNeutral(neutral + 1)
   const addBadFeedback = () => setBad(bad + 1)
+
+  let totalFeedback = good + neutral + bad
+
+  const average = () => {
+    if (good === 0 && neutral === 0 && bad === 0) return 0
+    return (good - bad) / totalFeedback
+  }
+
+  const positivePercent = () => {
+    if (good === 0 && neutral === 0 && bad === 0) return 0
+    return good / totalFeedback * 100
+  }
 
   return (
     <div>
@@ -27,9 +36,12 @@ const App = () => {
       <Button handleClick={addNeutralFeedback} text='neutral' />
       <Button handleClick={addBadFeedback} text='bad' />
       <Header title='statistics' />
-      <Statistic type='good' count={good} />
-      <Statistic type='neutral' count={neutral} />
-      <Statistic type='bad' count={bad} />
+      <Statistics type='good' count={good} />
+      <Statistics type='neutral' count={neutral} />
+      <Statistics type='bad' count={bad} />
+      <Statistics type='all' count={totalFeedback} />
+      <Statistics type='average' count={average()} />
+      <Statistics type='positive' count={positivePercent() + '%'} />
     </div>
   )
 }
