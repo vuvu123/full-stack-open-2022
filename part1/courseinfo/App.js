7aee12ab2e9069980f0c986e0222224d22e13fb3
Kenny <vuvu123@gmail.com>
2022-09-27 10:39:56 -0700

    Exercises 1.1-1.11 complete

--- part1/courseinfo/App.js
+++ part1/courseinfo/App.js
@@ -1,10 +1,8 @@
 const Header = (props) => {
-  // console.log(props)
   return <h1>{props.course}</h1>
 }
 
 const Content = (props) => {
-  console.log(props)
   return (
     <div>
     <Part name={props.parts[0].name} exercises={props.parts[0].exercises} />
