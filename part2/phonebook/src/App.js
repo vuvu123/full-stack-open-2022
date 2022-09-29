55470fabb8402a8cf891afbcae33b801ecca5fcd
vuvu123 <vuvu123@gmail.com>
2022-09-28 21:43:11 -0700

    2.9 checkpoint - working on filter

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -3,16 +3,18 @@ import Person from './components/Person'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { 
-      name: 'Arto Hellas', 
-      number: '040-123-4567',
-    }
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
   ]) 
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
+  const [newFilter, setNewFilter] = useState('')
 
   const handleNameChange = (event) => setNewName(event.target.value)
   const handleNumberChange = (event) => setNewNumber(event.target.value)
+  const handleFilterChange = (event) => setNewFilter(event.target.value)
   
   const addName = (event) => {
     event.preventDefault()
@@ -25,6 +27,7 @@ const App = () => {
     const nameObject = {
       name: newName,
       number: newNumber,
+      id: setPersons.length + 1
     }
     setPersons(persons.concat(nameObject))
     setNewName('')
@@ -35,7 +38,11 @@ const App = () => {
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>
+        filter shown with <input value={newFilter} onChange={handleFilterChange} />
+      </div>
       <form onSubmit={addName}>
+        <h2>add a new</h2>
         <div>
           name: <input value={newName} onChange={handleNameChange} />
         </div>
