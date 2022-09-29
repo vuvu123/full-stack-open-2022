3eec3d436e1b74998d982f60930b2a208a2095ed
vuvu123 <vuvu123@gmail.com>
2022-09-28 19:31:32 -0700

    exercise 2.6

---
+++ part2/phonebook/src/App.js
@@ -0,0 +1,41 @@
+import { useState } from 'react'
+import Person from './components/Person'
+
+const App = () => {
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas' }
+  ]) 
+  const [newName, setNewName] = useState('')
+
+  const handleNameChange = (event) => {
+    setNewName(event.target.value)
+  }
+
+  const addName = (event) => {
+    event.preventDefault()
+    const nameObject = {
+      name: newName,
+    }
+    setPersons(persons.concat(nameObject))
+    setNewName('')
+  }
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={addName}>
+        <div>
+          name: <input value={newName} onChange={handleNameChange} />
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      {persons.map(person => 
+      <Person key={person.name} name={person.name} />)}
+    </div>
+  )
+}
+
+export default App
---
+++ part2/phonebook/src/components/Person.js
@@ -0,0 +1,7 @@
+const Person = (props) => {
+  const { name } = props
+
+  return <div>{name}</div>
+}
+
+export default Person
