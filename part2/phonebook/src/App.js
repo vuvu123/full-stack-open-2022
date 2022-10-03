94ec70798cacbf01b69b900e81aba9f4cea5caae
Kenny <vuvu123@gmail.com>
2022-10-03 13:44:03 -0700

    exercise 2.15 (step 7)

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -51,12 +51,15 @@ const App = () => {
     }
     const newPerson = {
       name: newName,
-      number: newNumber,
-      id: persons.length + 1
+      number: newNumber
     }
-    setPersons(persons.concat(newPerson))
-    setNewName('')
-    setNewNumber('')
+    axios
+      .post('http://localhost:3001/persons', newPerson)
+      .then(response => {
+        setPersons(persons.concat(response.data))
+        setNewName('')
+        setNewNumber('')
+      })
   }
 
   const personsToShow = showAllPersons
