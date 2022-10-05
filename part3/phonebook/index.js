b0706dabd233a886511a45eb9de5607bcbc9d95f
Kenny <vuvu123@gmail.com>
2022-10-05 14:38:45 -0700

    Step 6: Post request error handling

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -65,10 +65,17 @@ const generateId = () => {
 
 app.post('/api/persons', (request, response) => {
   const body = request.body
+  const isDuplicatePerson = persons.find(person => person.name === body.name)
   
-  if (!body.name) {
+  if (!body.name || !body.number) {
     return response.status(400).json({
-      error: 'content missing'
+      error: 'name or number missing'
+    })
+  }
+
+  if (isDuplicatePerson) {
+    return response.status(400).json({
+      error: 'name must be unique'
     })
   }
   
