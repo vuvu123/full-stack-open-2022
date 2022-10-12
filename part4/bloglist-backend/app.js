2026c3cf045a26378ad87466b4a0c6d953f6fe69
Kenny <vuvu123@gmail.com>
2022-10-12 12:01:37 -0700

    Step 2: Refactor app into separate modules

---
+++ part4/bloglist-backend/app.js
@@ -0,0 +1,27 @@
+const config = require('./utils/config')
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const blogsRouter = require('./controllers/blogs')
+const middleware = require('./utils/middleware')
+const logger = require('./utils/logger')
+const mongoose = require('mongoose')
+
+logger.info('connecting to', config.MONGODB_URI)
+
+mongoose.connect(config.MONGODB_URI)
+  .then(() => {
+    logger.info('Connected to MongoDB')
+  })
+  .catch(e => {
+    logger.error('Error connecting to MongoDB: ', e.message)
+  })
+
+app.use(cors())
+app.use(express.json())
+app.use(middleware.requestLogger)
+app.use('/api/blogs', blogsRouter)
+app.use(middleware.unknownEndpoint)
+app.use(middleware.errorHandler)
+
+module.exports = app
