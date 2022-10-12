8cb8f0d55f9b3218c0b4467988313f7af7b08dec
Kenny <vuvu123@gmail.com>
2022-10-12 14:46:18 -0700

    Step 2: totalLikes test

--- part4/bloglist-backend/utils/list_helper.js
+++ part4/bloglist-backend/utils/list_helper.js
@@ -2,6 +2,15 @@ const dummy = (blogs) => {
   return 1
 }
 
+const totalLikes = (blogs) => {
+  return blogs.length === 0
+      ? 0
+      : blogs.reduce(
+        (prev, curr) => prev + curr.likes, 0
+      )
+}
+
 module.exports = {
-  dummy
+  dummy,
+  totalLikes,
 }
