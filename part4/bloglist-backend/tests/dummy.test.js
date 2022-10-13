18d00fe3b78de6e5bfe06c5ef254904d578ae47f
Kenny <vuvu123@gmail.com>
2022-10-13 07:31:11 -0700

    Step 3: favorite blog tests

--- part4/bloglist-backend/tests/dummy.test.js
+++
@@ -1,7 +0,0 @@
-const dummy = require('../utils/list_helper').dummy
-
-test('dummy returns one', () => {
-  const blogs = []
-  
-  expect(dummy(blogs)).toBe(1)
-})
--- part4/bloglist-backend/utils/list_helper.js
+++ part4/bloglist-backend/utils/list_helper.js
@@ -6,11 +6,24 @@ const totalLikes = (blogs) => {
   return blogs.length === 0
       ? 0
       : blogs.reduce(
-        (prev, curr) => prev + curr.likes, 0
+        (sum, curr) => sum + curr.likes, 0
       )
+}
+
+const favoriteBlog = (blogs) => {
+  if (blogs.length === 0) return null
+
+  const maxLikes = Math.max(...blogs.map(blog => blog.likes))
+  const mostLikedBlog = blogs.find(blog => blog.likes === maxLikes)
+  return {
+    title: mostLikedBlog.title,
+    author: mostLikedBlog.author,
+    likes: mostLikedBlog.likes
+  }
 }
 
 module.exports = {
   dummy,
   totalLikes,
+  favoriteBlog,
 }
