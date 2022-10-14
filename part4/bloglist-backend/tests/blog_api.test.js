3c6074b0437db3c402e0916244bed3769850c7a5
Kenny <vuvu123@gmail.com>
2022-10-14 14:03:57 -0700

    4.8 Step 1: Test returns correct amount of blog posts

---
+++ part4/bloglist-backend/tests/blog_api.test.js
@@ -0,0 +1,26 @@
+const supertest = require('supertest')
+const mongoose = require('mongoose')
+const app = require('../app')
+const api = supertest(app)
+
+const Blog = require('../models/blog')
+const helper = require('../utils/test_helper')
+
+beforeEach(async () => {
+  await Blog.deleteMany({})
+
+  const blogObjects = helper.initialBlogs
+    .map(blog => new Blog(blog))
+  const promiseArray = blogObjects.map(blog => blog.save())
+  await Promise.all(promiseArray)
+})
+
+test('all blogs are returned', async () => {
+  const response = await api.get('/api/blogs')
+
+  expect(response.body).toHaveLength(helper.initialBlogs.length)
+})
+
+afterAll(() => {
+  mongoose.connection.close()
+})
---
+++ part4/bloglist-backend/utils/test_helper.js
@@ -0,0 +1,33 @@
+const Blog = require('../models/blog')
+
+const initialBlogs = [
+  {
+    title: 'Web Dev Simplified',
+    author: 'Kyle',
+    url: 'http://blog.webdevsimplified.com',
+    likes: 69,
+  },
+  {
+    title: 'CSS Master',
+    author: 'Kevin Powell',
+    url: 'http://blog.kevinpowell.com',
+    likes: 13,
+  },
+]
+
+const nonExistingId = async () => {
+  const blog = new Blog({ title: 'removeme', author: 'R', url: 'w.com', likes: 10 })
+  await blog.save()
+  await blog.remove()
+
+  return blog._id.toString()
+}
+
+const blogsInDb = async () => {
+  const blogs = await Blog.find({})
+  return blogs.map(blog => blog.toJSON)
+}
+
+module.exports = {
+  initialBlogs, nonExistingId, blogsInDb
+}
