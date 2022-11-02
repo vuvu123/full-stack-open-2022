8086f9924e3db732441c311d3c54b1e7938cfdf9
Kenny <vuvu123@gmail.com>
2022-11-02 13:07:36 -0700

    5.7 Step 7: Toggle blog details

--- part5/bloglist-frontend/src/components/Blog.js
+++ part5/bloglist-frontend/src/components/Blog.js
@@ -1,7 +1,45 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} ➡ {blog.author} ➡ {blog.likes} likes
-  </div>  
-)
+import { useState } from "react"
+
+const Blog = ({ blog }) => {
+  const [visible, setVisible] = useState(false)
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+
+  const toggleVisibility = () => {
+    setVisible(!visible)
+  }
+
+  const addLike = () => {
+    console.log('Like button clicked')
+  }
+
+  const showBlogDetails = () => {
+    return (
+      <div>
+        <div>{blog.title} <button onClick={toggleVisibility}>hide</button></div>
+        <div>{blog.url}</div>
+        <div>likes: {blog.likes} <button onClick={addLike}>like</button></div>
+        <div>{blog.author}</div>
+      </div>
+    )
+  }
+
+  return (
+    <div style={blogStyle}>
+      { visible ?
+        showBlogDetails() :
+        <div>
+          {blog.title} - {blog.author}
+          <button onClick={toggleVisibility}>view</button>
+        </div> }
+    </div>  
+  )
+}
 
 export default Blog
