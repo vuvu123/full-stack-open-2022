667378bafc3cb701fae203e9be41d6cd3ea92900
vuvu123 <vuvu123@gmail.com>
2022-11-06 18:31:50 -0800

    5.14 Step 2: Test url/likes after button click

--- part5/bloglist-frontend/src/components/Blog.test.js
+++ part5/bloglist-frontend/src/components/Blog.test.js
@@ -24,3 +24,23 @@ test('renders title and author', () => {
   expect(likesElement).toBeNull()
   expect(urlElement).toBeNull()
 })
+
+test('checks blog url and likes are render after view button clicked', async () => {
+  const blog = {
+    title: 'Renders title',
+    author: 'Author',
+    likes: 10,
+    url: 'http://www.example.com'
+  }
+
+  render(<Blog blog={blog}  />)
+
+  const user = userEvent.setup()
+  const viewButton = screen.getByText('view')
+  await user.click(viewButton)
+
+  const urlElement = screen.getByText('http://www.example.com')
+  const likesElement = screen.getByText('likes: 10')
+  expect(urlElement).toBeDefined()
+  expect(likesElement).toBeDefined()
+})
