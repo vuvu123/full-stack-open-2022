d5351981afe073073c84e5c665e4d67b2cfe5c8a
Kenny <vuvu123@gmail.com>
2022-11-09 11:45:36 -0800

    Refactor LoginForm into separate component

--- part5/bloglist-frontend/src/App.js
+++ part5/bloglist-frontend/src/App.js
@@ -5,11 +5,10 @@ import BlogForm from './components/BlogForm'
 import blogService from './services/blogs'
 import loginService from './services/login'
 import Toggleable from './components/Toggleable'
+import LoginForm from './components/LoginForm'
 
 const App = () => {
   const [blogs, setBlogs] = useState([])
-  const [username, setUsername] = useState('')
-  const [password, setPassword] = useState('')
   const [user, setUser] = useState(null)
   const [message, setMessage] = useState(null)
 
@@ -28,19 +27,14 @@ const App = () => {
     }
   }, [])
 
-  const handleLogin = async event => {
-    event.preventDefault()
-
+  const handleLogin = async (username, password) => {
     try {
-      const user = await loginService.login({ username, password, })
-
+      const user = await loginService.login({ username, password })
       window.localStorage.setItem(
         'loggedBlogAppUser', JSON.stringify(user)
       )
       blogService.setToken(user.token)
       setUser(user)
-      setUsername('')
-      setPassword('')
     } catch (exception) {
       setMessage('wrong username or password')
       setTimeout(() => setMessage(null), 5000)
@@ -88,31 +82,8 @@ const App = () => {
     return (
       <div>
         <h2>Log in to application</h2>
-        <form onSubmit={handleLogin}>
-          <Notification message={message} />
-          <div>
-            username
-            <input
-              id='username'
-              type='text'
-              value={username}
-              name='Username'
-              onChange={({ target }) => setUsername(target.value)}
-            />
-          </div>
-          <div>
-            password
-            <input
-              id='password'
-              type='text'
-              value={password}
-              name='Password'
-              onChange={({ target }) => setPassword(target.value)}
-            />
-          </div>
-          <button type='submit'>login</button>
-
-        </form>
+        <Notification message={message} />
+        <LoginForm handleLogin={handleLogin}/>
       </div>
     )
   }
